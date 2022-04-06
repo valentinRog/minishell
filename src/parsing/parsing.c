54367c3cccd275de6402ac82a5f7a320aa45d262
Valentin Rogister <vrogiste@fu-r4-p1.s19.be>
2022-04-06 11:18:13 +0200

    yo

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 08:35:37 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/04/06 10:44:35 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/04/06 11:17:29 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,18 @@ void	parse_into_lst(t_list **alst, const char *line)
 {
 	char	*connector;
 	char	*cmd_line;
-	t_list	*node;
+	t_list	*new_node;
+	t_list	*cmd;
 
-	connector = str_tok(&cmd_line, line, , QUOTES);
-	
+	connector = str_tok(&cmd_line, line, get_connector_arr(), QUOTES);
+	if (cmd_line)
+	{
+		cmd = malloc(sizeof(cmd));
+		init_cmd(&cmd);
+		parse_cmd(cmd, cmd_line);
+		new_node = lst_new(cmd);
+		lst_add_back(alst, new_node);				
+	}
+	if (connector)
+		parse_into_lst(alst, line);
 }
