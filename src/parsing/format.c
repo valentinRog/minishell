cee8d7047a8a31bd17dac756ece5ea265630b6a3
Valentin Rogister <vrogiste@fu-r4-p1.s19.be>
2022-04-12 20:00:23 +0200

    yo

--- src/parsing/format.c
+++ src/parsing/format.c
@@ -6,79 +6,24 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 18:23:01 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/04/12 18:41:58 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/04/12 19:59:10 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	format_redirection(char **astr)
+static void	format_dst(char *str, char **adst)
 {
-	char	*ptr;
-	size_t	len;
-	char	**seps;
-
-	seps = split("<<:>>:>:<", ':');
-	ptr = (*astr);
-	while (*ptr)
-	{
-		len = con_len(*astr, ptr - *astr, seps, QUOTES);
-		if (len)
-		{
-			if (ptr > *astr && !is_in_str(WHITESPACES, *(ptr - 1)))
-			{
-				str_n_insert(astr, " ", (ptr) - *astr, 1);
-				return (format_redirection(astr));
-			}
-			ptr += len;
-			if (is_in_str(WHITESPACES, *ptr))
-			{
-				str_n_remove(astr, ptr - *astr, 1);
-				return (format_redirection(astr));
-			}
-		}
-		ptr++;
-	}
-	str_arr_free(seps);
-}
-
-void	format_space(char **astr)
-{
-	char	*ptr;
-	size_t	n;
-
-	ptr = *astr;
-	while (*ptr)
-	{
-		n = 0;
-		while (is_in_str(WHITESPACES, ptr[n])
-			&& !is_in_quote(*astr, QUOTES, ptr - *astr))
-			n++;
-		if (n > 1)
-		{
-			str_n_remove(astr, ptr - *astr, n - 1);
-			return (format_space(astr));
-		}
-		ptr++;
-	}
-}
-
-void	format_parenthesis(char **astr)
-{
-	char	*ptr;
-
-	if (is_in_str(WHITESPACES, **astr))
-		str_n_remove(astr, 0, 1);
+		
 }
 
 void	format(char **astr)
 {
-	format_space(astr);
-	format_redirection(astr);
-	if (errno == ENOMEM)
-	{
-		free(*astr);
-		*astr = NULL;
-	}
-	printf("%s\n", *astr);
+	char	*src;
+	char	*dst;
+
+	src = *astr;
+	dst = str_n_dup1();
+	free(src);
+	*astr = dst;
 }
