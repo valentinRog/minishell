526238abd7606446a441b9969aa5236764f7e2e4
Valentin Rogister <vrogiste@fu-r4-p1.s19.be>
2022-04-12 17:07:26 +0200

    yo

--- src/parsing/format.c
+++ src/parsing/format.c
@@ -6,13 +6,13 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 18:23:01 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/04/12 11:03:11 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/04/12 17:06:16 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	format_parentesis(char **astr)
+void	format_redirection(char **astr)
 {
 	char	*ptr;
 	size_t	len;
@@ -26,13 +26,13 @@ void	format_parentesis(char **astr)
 			if (ptr - 1 >= *astr && !is_in_str(WHITESPACES, *(ptr - 1)))
 			{
 				str_n_insert(astr, " ", (ptr) - *astr, 1);
-				return (format_parentesis(astr));
+				return (format_redirection(astr));
 			}
 			ptr += len;
 			if (is_in_str(WHITESPACES, *ptr))
 			{
 				str_n_remove(astr, ptr - *astr, 1);
-				return (format_parentesis(astr));
+				return (format_redirection(astr));
 			}
 		}
 		ptr++;
@@ -51,6 +51,8 @@ void	format_space(char **astr)
 		n = 0;
 		while (is_in_str(WHITESPACES, *(ptr + n)) && !is_in_quote(*astr, QUOTES, ptr - *astr))
 			n++;
+		if (ptr == *astr && *ptr == ' ')
+			n++;
 		if (n > 1)
 		{
 			str_n_remove(astr, ptr - *astr, n - 1);
@@ -64,6 +66,6 @@ void	format(char **astr)
 {
 	format_space(astr);
 	//format_redictions(astr);
-	format_parentesis(astr);
+	format_redirection(astr);
 	printf("%s\n", *astr);
 }
