abab76abfbb6797b2b5d20711f48286313525040
root <root@DESKTOP-INOLNT2.localdomain>
2022-04-14 07:18:59 +0200

    yo

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 08:35:37 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/04/13 22:57:54 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/04/14 07:18:10 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	parse_con(t_cmd *cmd, char *arg, char *con)
 {
 	cmd->con = get_con(con);
 }
-	
 
 bool	parse_arg(t_cmd *cmd, char *arg, char *con)
 {
@@ -25,15 +24,33 @@ bool	parse_arg(t_cmd *cmd, char *arg, char *con)
 	return (false);
 }
 
-static void	clear_parsing(t_list **alst, t_cmd *cmd, char *arg, char *con)
+static void	clear_parsing(t_list **alst, char *arg, char *con)
 {
 	lst_clear(alst, del_cmd);
-	del_cmd(cmd);
 	if (arg)
 		free(arg);
 	if (con)
 		free(con);
 	str_tok(NULL, NULL, NULL);
+}
+
+static t_cmd	*add_new_cmd(t_list **alst)
+{
+	t_cmd	*cmd;
+	t_list	*new_node;
+
+	cmd = malloc(sizeof(t_cmd));
+	if (!cmd)
+		return (NULL);
+	init_cmd(cmd);
+	new_node = lst_new(cmd);
+	if (!new_node)
+	{
+		del_cmd(cmd);
+		return (NULL);
+	}
+	lst_add_back(alst, new_node);
+	return (cmd);
 }
 
 static void	parse_into_lst(t_list **alst, char *line, t_tok *tok)
@@ -42,22 +59,24 @@ static void	parse_into_lst(t_list **alst, char *line, t_tok *tok)
 	char	*arg;
 	char	*con;
 
-	new_cmd(&cmd);
+	cmd = add_new_cmd(alst);
+	if (!cmd)
+		return (lst_clear(alst, del_cmd));
 	while (true)
 	{
 		arg = str_tok(&con, line, tok);
 		if (get_con(con))
 		{
 			cmd->con = get_con(con);
-			lst_add_back(alst, lst_new(cmd));
-			cmd = new_cmd(NULL);
+			cmd = add_new_cmd(alst);
+			if (!cmd)
+				return (clear_parsing(alst, arg, con));
 		}
 		if (!arg)
 			break ;
 		parse_arg(cmd, arg, con);
 		free(arg);
 	}
-	lst_add_back(alst, lst_new(cmd));
 	str_tok(NULL, NULL, NULL);
 }
 
