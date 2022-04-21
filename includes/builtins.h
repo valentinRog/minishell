a1b6f3f64bd54da5ccbda06e2894f62358e9c2e0
Bvernimm <bazvernim@outlook.fr>
2022-04-21 15:14:31 +0200

    cd et pwd

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: bvernimm <bvernimm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 17:17:41 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/04/21 10:27:03 by bvernimm         ###   ########.fr       */
+/*   Updated: 2022/04/21 15:10:37 by bvernimm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 void	ft_env(t_list *table[TABLE_SIZE]);
 void	ft_echo(char **cmd);
-void	ft_pwd();
+void	ft_pwd(void);
+void	ft_cd(char **cmd);
 
 #endif
