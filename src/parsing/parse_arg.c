a91c6124cd2df06a478ff1ac0d16170f655695aa
Valentin Rogister <vrogiste@fu-r4-p1.s19.be>
2022-04-25 10:31:22 +0200

    yo

--- src/parsing/parse_arg.c
+++ src/parsing/parse_arg.c
@@ -6,7 +6,7 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 10:43:41 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/04/22 18:09:03 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/04/25 10:30:24 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,38 @@ bool	redirection(t_cmd *cmd, char *arg, char *con)
 		free(arg_cpy);
 	if (!*arg || errno == ENOMEM)
 		return (true);
+	return (false);
+}
+
+int	z_index(enum e_z z)
+{
+	static int	z_index;
+
+	if (z == z_INCR)
+		z_index++;
+	else if (z == z_DECR)
+		z_index--;
+	else if (z == z_RESET)
+		z_index = 0;
+	return (z_index);
+}
+
+bool	parenthesis(t_cmd *cmd, char *arg, char *con)
+{
+	if (!str_cmp("(", con))
+	{
+		if (cmd->args || cmd->limiters || cmd->outfiles || cmd->infile)
+			return (true);
+		z_index(z_INCR);
+	}
+	if (!str_cmp(")", con))
+	{
+		z_index(z_DECR);
+		if (arg && *arg)
+			return (true);
+	}
+	else
+		cmd->z_index = z_index(z_NONE);
 	return (false);
 }
 
