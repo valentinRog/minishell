23e4283dabf76a0f9dcc1a787bc8cf35149a5edf
Valentin Rogister <vrogiste@fu-r4-p1.s19.be>
2022-04-26 18:58:04 +0200

    yo

--- src/exec/child.c
+++ src/exec/child.c
@@ -6,11 +6,17 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 14:43:11 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/04/26 18:27:38 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/04/26 18:57:39 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static bool	error(char *str)
+{
+	perror(str);
+	return (true);
+}
 
 static bool	dup_stdin(t_cmd *cmd, int i_pipe[2])
 {
@@ -23,6 +29,7 @@ static bool	dup_stdin(t_cmd *cmd, int i_pipe[2])
 		fd = open(cmd->infile, O_RDONLY);
 		if (fd < 0)
 		{
+			perror(cmd->infile);
 			close_pipe(i_pipe);
 			return (true);
 		}
@@ -37,24 +44,42 @@ static bool	dup_stdin(t_cmd *cmd, int i_pipe[2])
 	return (false);
 }
 
+static bool	dup_outfile(t_cmd *cmd, int o_pipe[2])
+{
+	t_list	*node;
+	int		fd;
+	int		ret;
+
+	node = cmd->outfiles;
+	while (node)
+	{
+		fd = open((char *)node->content, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+		if (fd < 0)
+		{
+			if (cmd->con == con_PIPE)
+				close_pipe(o_pipe);
+			return (error((char *)node->content));
+		}
+		if (node->next)
+			close(fd);
+		node = node->next;
+	}
+	ret = dup2(fd, STDOUT_FILENO);
+	close(fd);
+	if (cmd->con == con_PIPE)
+		close_pipe(o_pipe);
+	if (ret == -1)
+		return (error("dup2"));
+	return (false);
+}
+
 static bool	dup_stdout(t_cmd *cmd, int o_pipe[2])
 {
-	int	fd;
 	int	ret;
 
 	if (cmd->outfiles)
 	{
-		fd = open((char *)cmd->outfiles->content, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-		if (fd < 0)
-		{
-			close_pipe(o_pipe);
-			return (true);
-		}
-		ret = dup2(fd, STDOUT_FILENO);
-		close(fd);
-		if (cmd->con == con_PIPE)
-			close_pipe(o_pipe);
-		if (ret == -1)
+		if (dup_outfile(cmd, o_pipe))
 			return (true);
 	}
 	else if (cmd->con == con_PIPE)
@@ -73,7 +98,7 @@ void	child(t_cmd *cmd, int i_pipe[2], int o_pipe[2], t_list **alst)
 
 	if (dup_stdin(cmd, i_pipe) || dup_stdout(cmd, o_pipe))
 	{
-		perror("");
+		lst_clear(alst, del_cmd);
 		exit(EXIT_FAILURE);
 	}
 	if (is_tok((char *)cmd->args->content, "env:echo:pwd", ':'))
