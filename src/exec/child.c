bada3873767e50c830b66adf61a5f73d149febfb
Valentin Rogister <vrogiste@fu-r4-p1.s19.be>
2022-04-27 18:02:17 +0200

    yo

--- src/exec/child.c
+++ src/exec/child.c
@@ -6,7 +6,7 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 14:43:11 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/04/27 16:59:00 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/04/27 17:16:56 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	child(t_cmd *cmd, int i_pipe[2], int o_pipe[2], t_shell *shell)
 
 	if (dup_stdin(cmd, i_pipe, shell) || dup_stdout(cmd, o_pipe))
 	{
+		exec_error(NULL, shell, NULL, NULL);
 		exit(EXIT_FAILURE);
 	}
 	if (is_tok((char *)cmd->args->content, "env:echo:pwd", ':'))
