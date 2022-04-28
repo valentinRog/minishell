92a4ba433de1213fc95d347421c78fb6c404130f
Valentin Rogister <vrogiste@shi-r10-p6.s19.be>
2022-04-28 14:29:17 +0200

    yo

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,27 +6,58 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 12:45:34 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/04/28 13:26:16 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/04/28 14:28:33 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static char	**get_paths(t_shell *shell)
+{
+	t_list	*node;
+	char	**paths;
+
+	node = table_find(shell->table, "PATH");
+	if (!node)
+	{
+		exec_error(NULL, shell, NULL, NULL);
+		write(2, "PATH not found\n", str_len("PATH not found\n"));
+		exit(EXIT_FAILURE);
+	}
+	paths = split(((t_var *)node->content)->data, ':');
+	if (!paths)
+	{
+		exec_error("split", shell, NULL, NULL);
+		exit(EXIT_FAILURE);
+	}
+	return (paths);
+}
+
 void	exec_bin(char **cmds, t_shell *shell)
 {
-	char	**paths;
-	char	*cmd;
+	char		**paths;
+	char		*cmd;
+	size_t		i;
 
-	paths = split(((t_var *)table_find(shell->table, "PATH")->content)->data, ':');
-	while (*paths)
+	paths = get_paths(shell);
+	i = 0;
+	while (paths[i])
 	{
 		cmd = str_dup(*cmds);
 		str_n_insert(&cmd, "/", 0, 1);
-		str_n_insert(&cmd, *paths, 0, str_len(*paths));
-		execve(cmd, cmds, NULL);
-		paths++;
+		str_n_insert(&cmd, paths[i], 0, str_len(paths[i]));
+		if (!access(cmd, F_OK))
+		{
+			if (!access(cmd, X_OK))
+				execve(cmd, cmds, NULL);
+			exec_error(cmd, shell, NULL, NULL);
+			exit(126);
+		}
+		i++;
 	}
-	exit(EXIT_FAILURE);
+	exec_error(*cmds, shell, NULL, NULL);
+	str_arr_free(paths);
+	exit(127);
 }
 
 bool	exec_builtin(t_cmd *cmd, t_shell *shell)
