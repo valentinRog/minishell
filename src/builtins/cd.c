7cd442a215f059b2e0d0e8f81c0d69bfe6fae267
Valentin Rogister <vrogiste@fu-r4-p1.s19.be>
2022-04-28 11:37:40 +0200

    yo

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,36 +6,47 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 11:37:31 by bvernimm          #+#    #+#             */
-/*   Updated: 2022/04/28 10:31:00 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/04/28 11:37:10 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool	bi_cd(char **cmds)
+static char	*get_path(char **cmds)
 {
 	char	*path;
 	char	*user;
 
-	if (str_n_cmp(cmds[0], "cd", str_len(cmds[0])) != 0)
-		return (true);
 	if (cmds[1])
 	{
 		path = getcwd(NULL, 0);
 		str_n_insert(&path, "/", str_len(path), 1);
 		str_n_insert(&path, cmds[1], str_len(path), str_len(cmds[1]));
-		if (errno == ENOMEM)
-			return (true);
 	}
 	else
 	{
 		path = str_dup("/Users/");
 		user = getenv("USER");
 		str_n_insert(&path, user, str_len(path), str_len(user));
-		if (errno == ENOMEM)
-			return (true);
 	}
-	chdir(path);
+	return (path);
+}
+
+bool	bi_cd(char **cmds)
+{
+	char	*path;
+
+	if (!cmds || str_cmp("cd", *cmds))
+		return (true);
+	path = get_path(cmds);
+	if (!path)
+		return (exec_error("cd", NULL, NULL, NULL));
+	if (chdir(path) < 0)
+	{
+		perror(path);
+		free(path);
+		return (true);
+	}
 	free (path);
 	return (false);
 }
--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 09:52:21 by bvernimm          #+#    #+#             */
-/*   Updated: 2022/04/28 10:31:16 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/04/28 11:28:43 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,17 @@
 bool	bi_echo(char **cmds)
 {
 	int		i;
-	bool	n;
 
-	n = false;
-	i = 1;
-	if (str_n_cmp(cmds[0], "echo", str_len(cmds[0])) != 0)
+	if (!cmds || str_cmp("echo", *cmds))
 		return (true);
-	if (str_n_cmp(cmds[1], "-n", str_len(cmds[1])) == 0)
-	{
-		n = true;
-		i++;
-	}
+	i = 1;
 	while (cmds[i])
 	{
-		printf("%s", cmds[i]);
+		if (str_cmp(cmds[1], "-n") || i > 1)
+			printf("%s", cmds[i]);
 		i++;
 	}
-	if (n == false)
+	if (str_cmp(cmds[1], "-n"))
 		printf("\n");
 	return (false);
 }
