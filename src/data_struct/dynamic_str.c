d8e3d71d248cf75cc463b2852857f6ef0b2bd41a
valentinRog <valentin.rogister@gmail.com>
2022-05-05 18:29:31 +0200

    yo

--- src/data_struct/dynamic_str.c
+++ src/data_struct/dynamic_str.c
@@ -6,7 +6,7 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 14:02:11 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/05/05 14:41:22 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/05/05 18:23:34 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,37 +25,47 @@ t_dy_str	dy_str_new(void)
 
 void	dy_str_destroy(t_dy_str *dy_str)
 {
-	if (dy_str->alloc)
-	{
-		free(dy_str->str);
-		dy_str->alloc = 0;
-	}
+
+	free(dy_str->str);
+	dy_str->alloc = 0;
 }
 
 void	dy_str_grow(t_dy_str *dy_str)
 {
 	char	*new_str;
 
-	dy_str->alloc *= 2;
-	new_str = malloc(dy_str->alloc * sizeof(char));
-	str_n_cpy(new_str, dy_str->str, str_len(dy_str->str));
-	free(dy_str->str);
-	dy_str->str = new_str;
+	if (dy_str)
+	{
+		dy_str->alloc *= 2;
+		new_str = malloc(dy_str->alloc * sizeof(char));
+		if (new_str)
+			str_n_cpy(new_str, dy_str->str, str_len(dy_str->str));
+		free(dy_str->str);
+		dy_str->str = new_str;
+		if (!dy_str->str)
+			dy_str->alloc = 0;
+	}
 }
 
 void	dy_str_append_c(t_dy_str *dy_str, char c)
 {
 	if (str_len(dy_str->str) == dy_str->alloc - 1)
 		dy_str_grow(dy_str);
-	dy_str->str[str_len(dy_str->str) + 1] = '\0';
-	dy_str->str[str_len(dy_str->str)] = c;
+	if (dy_str->str)
+	{
+		dy_str->str[str_len(dy_str->str) + 1] = '\0';
+		dy_str->str[str_len(dy_str->str)] = c;
+	}
 }
 
 void	dy_str_append_str(t_dy_str *dy_str, char *str)
 {
-	while (str && *str)
+	if (dy_str && str)
 	{
-		dy_str_append_c(dy_str, *str);
-		str++;
+		while (*str)
+		{
+			dy_str_append_c(dy_str, *str);
+			str++;
+		}
 	}
 }
