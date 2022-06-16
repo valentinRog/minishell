7b71d11f3c8f2c27d98133b39f9eba58f971461d
valentinRog <valentin.rogister@gmail.com>
2022-06-16 17:23:27 +0200

    yo

--- src/exec/launcher.c
+++ src/exec/launcher.c
@@ -6,43 +6,36 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 05:12:36 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/06/16 17:20:59 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/06/16 17:22:30 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_cmd	*get_cmd(t_list *lst)
-{
-	if (lst)
-		return (lst->content);
-	return (NULL);
-}
-
-static t_list	*skip_higher_z_index(t_list *lst, int z)
+static t_list	*skip_higher_z(t_list *lst, int z)
 {
 	if (lst && lst->next && ((t_cmd *)lst->next->content)->z_index > z)
-		return (skip_higher_z_index(lst->next, z));
+		return (skip_higher_z(lst->next, z));
 	return (lst);
 }
 
 void	launcher(t_list *lst, int z, t_shell *shell)
 {
-	while (lst && get_cmd(lst)->z_index >= z)
+	while (lst && ((t_cmd *)lst->content)->z_index >= z)
 	{
-		if (get_cmd(lst)->z_index > z)
+		if (((t_cmd *)lst->content)->z_index > z)
 		{
 			launcher(lst, z + 1, shell);
-			lst = skip_higher_z_index(lst, z);
+			lst = skip_higher_z(lst, z);
 		}
 		else
 			pipex(lst, NULL, shell);
-		while (get_cmd(lst)->con == con_PIPE)
+		while (((t_cmd *)lst->content)->con == con_PIPE)
 			lst = lst->next;
-		while (lst && g_exit_code && get_cmd(lst)->con == con_AND)
-			lst = skip_higher_z_index(lst->next, z);
-		while (lst && (!g_exit_code && get_cmd(lst)->con == con_OR))
-			lst = skip_higher_z_index(lst->next, z);
+		while (lst && g_exit_code && ((t_cmd *)lst->content)->con == con_AND)
+			lst = skip_higher_z(lst->next, z);
+		while (lst && (!g_exit_code && ((t_cmd *)lst->content)->con == con_OR))
+			lst = skip_higher_z(lst->next, z);
 		lst = lst->next;
 	}
 }
