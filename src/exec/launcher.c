6300508d0493b8246be6885c6b0f53961b76dc25
valentinRog <valentin.rogister@gmail.com>
2022-06-16 17:21:22 +0200

    yo

--- src/exec/launcher.c
+++ src/exec/launcher.c
@@ -6,34 +6,43 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 05:12:36 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/05/08 22:35:26 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/06/16 17:20:59 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static t_cmd	*get_cmd(t_list *lst)
+{
+	if (lst)
+		return (lst->content);
+	return (NULL);
+}
+
+static t_list	*skip_higher_z_index(t_list *lst, int z)
+{
+	if (lst && lst->next && ((t_cmd *)lst->next->content)->z_index > z)
+		return (skip_higher_z_index(lst->next, z));
+	return (lst);
+}
+
 void	launcher(t_list *lst, int z, t_shell *shell)
 {
-	while (lst && ((t_cmd *)lst->content)->z_index >= z)
+	while (lst && get_cmd(lst)->z_index >= z)
 	{
-		if (((t_cmd *)lst->content)->z_index > z)
+		if (get_cmd(lst)->z_index > z)
 		{
 			launcher(lst, z + 1, shell);
-			while
-				(
-				lst && lst->next
-				&& ((t_cmd *)lst->next->content)->z_index > z
-				)
-					lst = lst->next;
+			lst = skip_higher_z_index(lst, z);
 		}
 		else
 			pipex(lst, NULL, shell);
-		while (((t_cmd *)lst->content)->con == con_PIPE)
+		while (get_cmd(lst)->con == con_PIPE)
 			lst = lst->next;
-		while (lst && g_exit_code && ((t_cmd *)lst->content)->con == con_AND)
-			lst = lst->next;
-		if (!lst || (!g_exit_code && ((t_cmd *)lst->content)->con == con_OR))
-			break ;
+		while (lst && g_exit_code && get_cmd(lst)->con == con_AND)
+			lst = skip_higher_z_index(lst->next, z);
+		while (lst && (!g_exit_code && get_cmd(lst)->con == con_OR))
+			lst = skip_higher_z_index(lst->next, z);
 		lst = lst->next;
 	}
 }
